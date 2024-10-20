578bbb1d20aa8754f481a0ecc95f23f95ee9495f
Adenekan123 <mr.nekan123@gmail.com>
2024-10-20 15:06:29 +0100

    added fixed header

--- src/routes/_layouts/dashboard.tsx
+++ src/routes/_layouts/dashboard.tsx
@@ -12,14 +12,14 @@ export const DashboardLayout = () => {
       </aside>
       <main className="col-span-12 lg:col-span-10 pb-24 h-full">
         <div className="flex flex-col">
-          <div className="h-[70px] bg-white px-5 md:px-24 shadow-sm flex justify-between items-center">
+          <div className="h-[70px] bg-white px-5 md:px-24 shadow-sm flex justify-between items-center fixed left-0 top-0 z-10 w-full">
             <h2 className="font-semibold">Dashboard</h2>
             <div className="flex gap-8 items-center">
               <TbBell className="h-6 w-6 md:h-7 md:w-7" />
               <TbUserSquareRounded  className="h-6 w-6 md:h-7 md:w-7" />
             </div>
           </div>
-          <div className="flex-1 bg-red px-5 md:px-24 py-6 h-[calc(100vh-70px)]">
+          <div className="flex-1 bg-red px-5 md:px-24 py-6 mt-[70px] h-[calc(100vh-70px)] overflow-hidden overflow-y-auto bg-bgBase">
             <Outlet />
           </div>
         </div>
