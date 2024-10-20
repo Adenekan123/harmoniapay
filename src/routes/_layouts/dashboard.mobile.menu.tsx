74fde192f92a9ed199790e75d5d8581f75f1fd2f
Adenekan123 <mr.nekan123@gmail.com>
2024-10-20 13:18:14 +0100

    sign in page update III

--- src/routes/_layouts/dashboard.mobile.menu.tsx
+++ src/routes/_layouts/dashboard.mobile.menu.tsx
@@ -83,7 +83,7 @@ const MobileMenu = () => {
     });
   }, [pathname]);
   return (
-    <div className="grid grid-cols-5 gap-6 items-center justify-center lg:hidden px-4">
+    <div className="grid grid-cols-5 gap-6 items-center justify-center lg:hidden px-5">
       {menunavContent}
     </div>
   );
